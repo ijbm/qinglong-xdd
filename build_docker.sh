106f635a958c4e5550cad0259c8e63a811be6551
gcdd1993 <gcwm99@gmail.com>
2021-08-18 15:05:03 +0800

    完成测试

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,8 @@
 version=v$(date "+%Y%m%d")
 
 set -ex
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -f Dockerfile --build-arg QL_VERSION="$QL_VERSION" -t gcdd1993/qinglong-xdd:latest -t gcdd1993/qinglong-xdd:"$version" .
-#docker push gcdd1993/qinglong-xdd:latest
-#docker push gcdd1993/qinglong-xdd:"$version"
+docker push gcdd1993/qinglong-xdd:latest
+docker push gcdd1993/qinglong-xdd:"$version"
